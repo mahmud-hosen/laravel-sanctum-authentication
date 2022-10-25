2cfb2d75e89060614ed6afd2372a80aee5b31a1a
mahmud-hosen <mahmudhossain582@gmail.com>
2022-10-25 22:12:46 +0600

    Added

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'id' => 1,
+            'name' => 'Car',
+            'price' => 500,
+            'description' => 'It is good product',
+       ]);
+        
+    }
+}
